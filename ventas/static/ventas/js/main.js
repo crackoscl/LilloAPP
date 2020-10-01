f28dad26ccf39a51b13fdd1b4c195d5c99702cae
crackoscl <gilbert.ata@gmail.com>
2020-09-30 21:02:53 -0300

    Mejora ya no se me ocurre nada \:\

--- ventas/static/ventas/js/main.js
+++ ventas/static/ventas/js/main.js
@@ -1,4 +1,4 @@
-const GetAllSales = 'jsonapi';
+// const GetAllSales = 'jsonapi';
 const GetSaleofDay = 'jsonapi/ventasdeldia'
 const GetSaleLastSevenDays = 'jsonapi/ventassietedias'
 const GetSalesOfMont = 'jsonapi/ventasdelmes'
@@ -47,234 +47,79 @@ const CreateFilter = data => data.reduce((acc, el) => ({
 const SumValues = data => data.reduce((acc, el) => acc + el)
 
 
-function SalesOfDay() {
+const SalesOf = (url, IdDiv) => {
 
-    fetch(GetSaleofDay)
-        .then(response => response.json())
-        .then(SalesOfDayData)
-}
+    fetch(url).then(response => response.json()).then(data => {
 
-const SalesOfDayData = (data) => {
+        if (data.ventas.length) {
 
-    if (data.ventas.length) {
+            var ctx = IdDiv
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: prod = Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas'],
+                    datasets: [{
+                        label: 'Número de Ventas',
+                        data: total = Object.values(CreateFilter(data.ventas)), //[10,20,30,20],
+                        backgroundColor: backgroundColor,
+                        borderColor: borderColor,
+                        borderWidth: 1
+                    }]
+                },
+                options: options
+            });
 
-        var ctx = iDSalesofDay
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: prod = Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas'],
-                datasets: [{
-                    label: 'Número de Ventas',
-                    data: total = Object.values(CreateFilter(data.ventas)), //[10,20,30,20],
-                    backgroundColor: backgroundColor,
-                    borderColor: borderColor,
-                    borderWidth: 1
-                }]
-            },
-            options: options
-        });
-
-    } else {
-        let ctx = iDSalesofDay
-        ctx.font = "22px sans-serif";
-        ctx.fillText(" No hay datos!", 50, 200);
-    }
+        } else {
+            let ctx = IdDiv
+            ctx.font = "22px sans-serif";
+            ctx.fillText(" No hay datos!", 50, 200);
+        }
+    })
 
 }
 
-function SalesOfDayPercent() {
-    fetch(GetSaleofDay)
-        .then(response => response.json())
-        .then(SalesOfDayPercentData)
-}
+const SalesOfPercent = (url, IdDiv) => {
+
+    fetch(url).then(response => response.json()).then(data => {
+
+        if (data.ventas.length) {
+
+            const DataValue = Object.values(CreateFilter(data.ventas))
 
 
-function SalesOfDayPercentData(data) {
+            const ValuesMap = DataValue.map(values => values / SumValues(DataValue) * 100)
 
-    if (data.ventas.length) {
+            var ctx = IdDiv
+            new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: prod = Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas'],
+                    datasets: [{
+                        label: '% de  Ventas',
+                        data: ValuesMap, //[10,20,30,20],
+                        backgroundColor: backgroundColor,
+                        borderColor: borderColor,
+                        borderWidth: 1
+                    }]
+                },
+                options: options
+            });
 
-        const DataValue = Object.values(CreateFilter(data.ventas))
+        } else {
+            let ctx = IdDiv
+            ctx.font = "22px sans-serif";
+            ctx.fillText(" No hay datos!", 50, 200);
+        }
 
-
-        const ValuesMap = DataValue.map(values => values / SumValues(DataValue) * 100)
-
-        var ctx = iDSalesofDayPorcent
-        new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: prod = Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas'],
-                datasets: [{
-                    label: '% de  Ventas',
-                    data: ValuesMap, //[10,20,30,20],
-                    backgroundColor: backgroundColor,
-                    borderColor: borderColor,
-                    borderWidth: 1
-                }]
-            },
-            options: options
-        });
-
-    } else {
-        let ctx = iDSalesofDayPorcent
-        ctx.font = "22px sans-serif";
-        ctx.fillText(" No hay datos!", 50, 200);
-    }
-}
-
-function SaleofLastSeventDays() {
-
-    fetch(GetSaleLastSevenDays)
-        .then(response => response.json())
-        .then(SaleofLastSeventDaysData)
-
-}
-
-function SaleofLastSeventDaysData(data) {
-
-
-    if (data.ventas.length) {
-
-        var ctx = IDLastSevenDaysSales
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: prod = Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas']
-                datasets: [{
-                    label: 'Número de Ventas',
-                    data: total = Object.values(CreateFilter(data.ventas)), //[10,20,30,20],
-                    backgroundColor: backgroundColor,
-                    borderColor: borderColor,
-                    borderWidth: 1
-                }]
-            },
-            options: options
-        });
-    } else {
-        let ctx = IDLastSevenDaysSales
-        ctx.font = "22px sans-serif";
-        ctx.fillText(" No hay datos!", 50, 200);
-    }
-}
-
-
-function SaleofLastSeventDaysPercent() {
-
-
-    fetch(GetSaleLastSevenDays)
-        .then(response => response.json())
-        .then(SaleofLastSeventDaysPercentData)
-}
-
-
-function SaleofLastSeventDaysPercentData(data) {
-
-    if (data.ventas.length) {
-
-        const DataValue = Object.values(CreateFilter(data.ventas))
-
-        const ValuesMap = DataValue.map(values => values / SumValues(DataValue) * 100)
-
-        var ctx = IDLastSevenDaysSalesPercent
-        new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: prod = Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas']
-                datasets: [{
-                    label: '% de  Ventas',
-                    data: ValuesMap, //[10,20,30,20],
-                    backgroundColor: backgroundColor,
-                    borderColor: borderColor,
-                    borderWidth: 1
-                }]
-            },
-            options: options
-        });
-    } else {
-        let ctx = IDLastSevenDaysSalesPercent
-        ctx.font = "22px sans-serif";
-        ctx.fillText(" No hay datos!", 50, 200);
-    }
-
-}
-
-function SaleOfMonth() {
-
-    fetch(GetSalesOfMont)
-        .then(response => response.json())
-        .then(SaleOfMonthData)
-}
-
-
-function SaleOfMonthData(data) {
-    if (data.ventas.length) {
-
-        var ctx = IdSalesMonth
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas'],
-                datasets: [{
-                    label: 'Número de Ventas',
-                    data: total = Object.values(CreateFilter(data.ventas)), //[10,20,30,20],
-                    backgroundColor: backgroundColor,
-                    borderColor: borderColor,
-                    borderWidth: 1
-                }]
-            },
-            options: options
-        });
-    } else {
-        let ctx = IdSalesMonth
-        ctx.font = "22px sans-serif";
-        ctx.fillText(" No hay datos!", 50, 200);
-    }
-}
-
-
-function SaleOfMonthPercent() {
-
-
-    fetch(GetSalesOfMont)
-        .then(response => response.json())
-        .then(SaleOfMonthpercentData)
-}
-
-function SaleOfMonthpercentData(data) {
-
-    if (data.ventas.length) {
-
-        const DataValue = Object.values(CreateFilter(data.ventas))
-
-        const ValuesMap = DataValue.map(values => values / SumValues(DataValue) * 100)
-
-        var ctx = IdSalesMonthPercent
-        new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: prod = Object.keys(CreateFilter(data.ventas)), //['completo', 'fajita', 'ass', 'bebidas'],
-                datasets: [{
-                    label: '% de  Ventas',
-                    data: ValuesMap, //[10,20,30,20],
-                    backgroundColor: backgroundColor,
-                    borderColor: borderColor,
-                    borderWidth: 1
-                }]
-            },
-            options: options
-        });
-    } else {
-        let ctx = IdSalesMonthPercent
-        ctx.font = "22px sans-serif";
-        ctx.fillText(" No hay datos!", 50, 200);
-    }
+    })
 
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    SalesOfDay()
-    SalesOfDayPercent()
-    SaleofLastSeventDays()
-    SaleofLastSeventDaysPercent()
-    SaleOfMonth()
-    SaleOfMonthPercent()
+    SalesOf(GetSaleofDay, iDSalesofDay);
+    SalesOf(GetSaleLastSevenDays, IDLastSevenDaysSales)
+    SalesOf(GetSalesOfMont, IdSalesMonth)
+    SalesOfPercent(GetSaleofDay, iDSalesofDayPorcent)
+    SalesOfPercent(GetSaleLastSevenDays, IDLastSevenDaysSalesPercent)
+    SalesOfPercent(GetSalesOfMont, IdSalesMonthPercent)
 });
